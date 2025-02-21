c727137bac7265ea47d37d473d525b3ff127180b
Ericprog01 <ericfreitas14a@gmail.com>
2025-02-21 15:16:24 +0000

    Criei a CARTA01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,55 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+            
+    printf("Carta01 \n");
+            
+    char estado1;
+    char codigo1[5];
+    char cidade1[50];
+    float populacao1;
+    float area1;
+    float pib1;
+    float pontos_turisticos1;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    printf("*** DIGITE OS DADOS DA CARTA01 ***\n");
+    printf("Estado (A-H): \n");
+    scanf("%c", &estado1);
+
+    printf("Codigo da carta (A01, B01): \n");
+    scanf("%s", &codigo1);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", cidade1);
+
+    printf("População: \n");
+    scanf("%f", populacao1);
+
+    printf("Área em km²: \n");
+    scanf("%f", area1);
+
+    printf("Pib: \n");
+    scanf("%f", pib1);
+    printf("Pontos Turisticos: \n");
+    scanf("%i", pontos_turisticos1);
+
+    printf("Carta02 \n");
+
+    char estado2;
+    char codigo2[5];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos_turisticos2;
+
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
+
+
+
+
+
+
 }
